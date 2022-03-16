16e8b9cc7b0231501c976d299c8960a4cacc0de4
franzannakarolina <nerdstecnologia0@gmail.com>
2022-03-15 21:20:47 -0300

    finalizado landing page web

--- src/styles/globalstyles.ts
+++ src/styles/globalstyles.ts
@@ -8,17 +8,13 @@ export const GlobalStyles = createGlobalStyle`
   padding: 0;
   box-sizing: border-box;
  }
-
-
 body{
   font-family: "Heebo", sans-serif;
   -webkit-font-smoothing: antialiased;
   background-color: var(--background);
  }
-
 :root {
   /* Colors */
-
   --space: #0B1E8A;
   --space-ligth: #3D68B2;
   --space-dark: #040327;
@@ -42,23 +38,18 @@ body{
   
   /* Fonts */
     --font-display: 800 ${pixelToRem(62)}/${pixelToRem(97)} "Heebo", sans-serif;
-
     --font-heading-1: 700 ${pixelToRem(32)}/${pixelToRem(
   48
 )} "Heebo", sans-serif;
-
     --font-heading-2: 500 ${pixelToRem(24)}/${pixelToRem(
   32
 )} "Heebo", sans-serif;
-
     --font-heading-3: 400 ${pixelToRem(20)}/${pixelToRem(
   24
 )} "Heebo", sans-serif;
-
 --text-4: 500 ${pixelToRem(18)}/${pixelToRem(32)} "Heebo", sans-serif;
 --text-3: 500 ${pixelToRem(18)}/${pixelToRem(24)} "Heebo", sans-serif;
 --text-2: 500 ${pixelToRem(14)}/${pixelToRem(20)} "Heebo", sans-serif;
 --text-1: 400 ${pixelToRem(16)}/${pixelToRem(24)} "Heebo", sans-serif;
 --text-0: 400 ${pixelToRem(14)}/${pixelToRem(24)} "Heebo", sans-serif;
-
 }`;
